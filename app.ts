6a2994d8f066cbc9f6457fa0d32d34db010412d2
vincentferraro <v.ferraro@live.fr>
2023-11-09 10:02:10 -0500

    add setName function add better displaying username

--- app.ts
+++ app.ts
@@ -1,21 +1,51 @@
-// import express, { Express } from "express";
-import http from 'http'
+import express from "express";
+import { createServer} from 'http'
 import { Server } from "socket.io";
 // import { join } from 'path'
 
 // const app : Express= express()
-const httpServer = http.createServer()
+const app  = express()
+const httpServer = createServer(app)
 const port: number = 3000
+const host: string = 'http://localhost'
 const io: Server = new Server(httpServer)
 
+io.socketsJoin("room1")
+
+function addName(){
+  let count = 0
+  let listName=["Alice", "Henry","Georges"]
+  return ()=>{
+    let name= listName[count]
+    count = count === 2? 0 : count+=1
+    return name
+
+  }
+}
+
+const setName= addName()
+
+// ON CONNECTION
 io.on('connection', (socket) => {
-    console.log('A user connected');
+    console.log('A user connected', socket.rooms);
+    socket.join("room1")
+    console.log("ROOM:", socket.rooms)
+    socket.data.username = setName()
+
+    console.log("NAME",socket.data.username)
+    //
+    // ON DISCONNECT
+    //
+
     socket.on('disconnect', () => {
       console.log('A user disconnected');
     });
 
+    //
+    // ON CHAT MESSAGE
+    //
     socket.on('chat message', (msg) => {
-        console.log(`${socket.id}`,msg.toString().trim())
+        console.log(`${socket.data.username} :`,msg.toString().trim())
     });
 
   });
