3d580acbb5987f285cf18215cc4e999b53ba074e
vincentferraro <v.ferraro@live.fr>
2023-12-03 21:55:46 -0500

    Better disconnection management

--- src/sockets/on/disconnection.ts
+++ src/sockets/on/disconnection.ts
@@ -1,14 +1,16 @@
 import {  Socket } from "socket.io";
 import { handleUserRoomDisconection } from "../../redis/redis";
-import emitGetRooms from "../emit/emitGetRoom";
-import getUsersRoom from "./getUsersRoom";
-import emitUsersRoom from "../emit/emitUsersRoom";
-
-export default function disconnection(socket: Socket, client: any): void{
+// import emitGetRooms from "../emit/emitGetRoom";
+// import getUsersRoom from "./getUsersRoom";
+// import emitUsersRoom from "../emit/emitUsersRoom";
+import emitUpdateUserList from "../emit/emitUpdateUserList";
+export default function disconnection(socket: Socket, redis: any): void{
     socket.on('disconnect', ():void => {
       const rooms = ['general', 'room1', 'room2']
-      handleUserRoomDisconection(client,rooms,{id:socket.id,username: socket.data.username})
-      rooms.forEach((room)=> socket.leave(room))
+      handleUserRoomDisconection(redis,rooms,{id:socket.id,username: socket.data.username})
+      
+      rooms.forEach((room)=> {socket.leave(room)
+        emitUpdateUserList( redis,room)})
         console.log(`${socket.data.username} disconected`);
 
       });
